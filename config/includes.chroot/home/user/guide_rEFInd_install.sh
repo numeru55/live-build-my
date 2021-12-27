134c1b8169599de6ad700ed21b5bf074f1c7f257
numeru55 <89648361+numeru55@users.noreply.github.com>
2021-12-27 22:25:59 +0900

    Create guide_rEFInd_install.sh

---
+++ config/includes.chroot/home/user/guide_rEFInd_install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# wget https://sourceforge.net/projects/refind/files/0.13.2/refind-bin-0.13.2.zip/download -O refind-bin-0.13.2.zip
+# unzip refind-bin-0.13.2.zip
+
+EFI=/dev/sdc1
+
+lsblk
+
+echo "===== will fix EFI to... ====="
+
+echo "   EFI:" $EFI
+
+echo "====="
+
+read -p "[yes]+[return] to go:" keyboard
+
+if [ "$keyboard" != "yes" ]; then
+    echo "skipped."
+    exit 1;
+fi
+
+echo "Proceeding..."
+
+mkdir efi
+sudo mount $EFI efi
+sudo mkdir -p efi/EFI/BOOT
+
+sudo cp -r refind-bin-*/refind/* efi/EFI/BOOT/
+
+sudo mv efi/EFI/BOOT/refind_x64.efi efi/EFI/BOOT/bootx64.efi 
+
+sudo cp my_refind.conf efi/EFI/BOOT/
+
+sudo umount efi
+
+echo "done."
